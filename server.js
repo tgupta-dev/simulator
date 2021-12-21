2b24d988f84587e91e305fda630f927ddad1c0f0
Tushar Gupta <tushar_gupta@anfcorp.com>
2021-12-21 14:17:03 -0500

    added default json

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const app = express();
 const port = 3010;
 const fs = require('fs');
 
-app.get('/content', (req, res) => {
-
-    let jsonObj = fs.readFileSync(`content/${req.query.espotId}.json`);
-    let content = JSON.parse(jsonObj);
-    res.send(content);
+app.get('/api/cms/bin/content', (req, res) => {
+  let jsonObj;
+  try {
+    jsonObj = fs.readFileSync(`content/${req.query.espotId}.json`);
+  } catch (err) {
+    jsonObj = fs.readFileSync('content/default.json');
+  }
+    
+  let content = JSON.parse(jsonObj);
+  res.send(content);
 });
 
 app.listen(port, () => {
